f8519b27a5c3f8ce23a3fc6a90c4a5354a4997da
leon_2192 <leon.strokes@gmail.com>
2024-10-31 16:14:32 -0300

    feat constants + bg color tasks en base a response api

--- src/components/shared/TaskList/TaskList.tsx
+++ src/components/shared/TaskList/TaskList.tsx
@@ -6,9 +6,13 @@ import {
   Divider,
   Checkbox,
   Button,
+  Select,
+  MenuItem,
+  FormControl,
 } from "@mui/material";
 import { useGlobalContext } from "../../../utilities/hooks/useGlobalContext";
 import { useNavigate } from "react-router-dom";
+import statusTasks from "../../../constants/status.json";
 
 const TaskList: React.FC = () => {
   const { tasks, setTasks, categories } = useGlobalContext();
@@ -18,9 +22,11 @@ const TaskList: React.FC = () => {
   const pendingTasks = tasks.filter((task) => !task.completed);
   const completedTasks = tasks.filter((task) => task.completed);
 
-  const getCategoryName = (categoryId: string) => {
-    const category = categories.find((cat) => cat.id === categoryId);
-    return category ? category.name : "Sin categoría";
+  const getCategory = (categoryId: string) => {
+    return categories.find((cat) => cat.id === categoryId) || {
+      name: "Sin categoría",
+      color: "#f0f0f0", 
+    };
   };
 
   const handleSelectTask = (taskId: string) => {
@@ -31,10 +37,10 @@ const TaskList: React.FC = () => {
     navigate(`/task/${taskId}`);
   };
 
-  const handleToggleComplete = (taskId: string) => {
+  const handleChangeStatus = (taskId: string, completed: boolean) => {
     setTasks((prevTasks) =>
       prevTasks.map((task) =>
-        task.id === taskId ? { ...task, completed: !task.completed } : task
+        task.id === taskId ? { ...task, completed } : task
       )
     );
   };
@@ -78,55 +84,67 @@ const TaskList: React.FC = () => {
         </Typography>
       </Box>
       {pendingTasks.length > 0 ? (
-        pendingTasks.map((task) => (
-          <Paper
-            key={task.id}
-            sx={{
-              width: "100%",
-              maxWidth: 800,
-              padding: 2,
-              marginBottom: 2, 
-              textAlign: "left",
-              border: selectedTaskId === task.id ? "2px solid #1976d2" : "none",
-              backgroundColor:
-                selectedTaskId === task.id ? "#e3f2fd" : "inherit",
-            }}
-          >
-            <Box sx={{ display: "flex", alignItems: "center" }}>
-              <Checkbox
-                checked={selectedTaskId === task.id}
-                onChange={() => handleSelectTask(task.id)}
-                inputProps={{ "aria-label": "checkbox for task" }}
-              />
-              <Box sx={{ flexGrow: 1 }}>
-                <Typography variant="h6">{task.title}</Typography>
-                <Typography variant="body2" color="text.secondary">
-                  {getCategoryName(task.category_id)}:{" "}
-                  {task.description || "Sin descripción"}
-                </Typography>
-              </Box>
-              {selectedTaskId === task.id && (
-                <Box sx={{ display: "flex", gap: 1 }}>
-                  <Button
-                    variant="outlined"
-                    color="primary"
-                    onClick={() => handleEditClick(task.id)}
-                  >
-                    Editar
-                  </Button>
-                  <Button
-                    variant="contained"
-                    color="secondary"
-                    onClick={() => handleToggleComplete(task.id)}
-                  >
-                    Finalizar
-                  </Button>
+        pendingTasks.map((task) => {
+          const { color } = getCategory(task.category_id);
+          return (
+            <Paper
+              key={task.id}
+              sx={{
+                width: "100%",
+                maxWidth: 800,
+                padding: 2,
+                marginBottom: 2,
+                textAlign: "left",
+                border: selectedTaskId === task.id ? "2px solid #1976d2" : "none",
+                backgroundColor: selectedTaskId === task.id ? "#e3f2fd" : color, 
+              }}
+            >
+              <Box sx={{ display: "flex", alignItems: "center" }}>
+                <Checkbox
+                  checked={selectedTaskId === task.id}
+                  onChange={() => handleSelectTask(task.id)}
+                  inputProps={{ "aria-label": "checkbox for task" }}
+                />
+                <Box sx={{ flexGrow: 1 }}>
+                  <Typography variant="h6">{task.title}</Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    {getCategory(task.category_id).name}:{" "}
+                    {task.description || "Sin descripción"}
+                  </Typography>
                 </Box>
-              )}
-            </Box>
-            
-          </Paper>
-        ))
+                {selectedTaskId === task.id && (
+                  <Box sx={{ display: "flex", gap: 1 }}>
+                    <Button
+                      variant="outlined"
+                      color="primary"
+                      onClick={() => handleEditClick(task.id)}
+                    >
+                      Editar
+                    </Button>
+                    <FormControl variant="outlined" size="small">
+                      <Select
+                        labelId={`status-select-label-${task.id}`}
+                        value={task.completed ? "completed" : "pending"}
+                        onChange={(e) =>
+                          handleChangeStatus(
+                            task.id,
+                            e.target.value === "completed"
+                          )
+                        }
+                      >
+                        {statusTasks.statuses.map((status) => (
+                          <MenuItem key={status.id} value={status.id}>
+                            {status.label}
+                          </MenuItem>
+                        ))}
+                      </Select>
+                    </FormControl>
+                  </Box>
+                )}
+              </Box>
+            </Paper>
+          );
+        })
       ) : (
         <Typography variant="body1">No hay tareas pendientes.</Typography>
       )}
@@ -147,55 +165,67 @@ const TaskList: React.FC = () => {
         </Typography>
       </Box>
       {completedTasks.length > 0 ? (
-        completedTasks.map((task) => (
-          <Paper
-            key={task.id}
-            sx={{
-              width: "100%",
-              maxWidth: 800,
-              padding: 2,
-              marginBottom: 2, // Agregar margen inferior
-              textAlign: "left",
-              border: selectedTaskId === task.id ? "2px solid #1976d2" : "none",
-              backgroundColor:
-                selectedTaskId === task.id ? "#e0f7fa" : "#f1f8e9",
-            }}
-          >
-            <Box sx={{ display: "flex", alignItems: "center" }}>
-              <Checkbox
-                checked={selectedTaskId === task.id}
-                onChange={() => handleSelectTask(task.id)}
-                inputProps={{ "aria-label": "checkbox for completed task" }}
-              />
-              <Box sx={{ flexGrow: 1 }}>
-                <Typography variant="h6">{task.title}</Typography>
-                <Typography variant="body2" color="text.secondary">
-                  {getCategoryName(task.category_id)}:{" "}
-                  {task.description || "Sin descripción"}
-                </Typography>
-              </Box>
-              {selectedTaskId === task.id && (
-                <Box sx={{ display: "flex", gap: 1 }}>
-                  <Button
-                    variant="outlined"
-                    color="primary"
-                    onClick={() => handleEditClick(task.id)}
-                  >
-                    Editar
-                  </Button>
-                  <Button
-                    variant="contained"
-                    color="secondary"
-                    onClick={() => handleToggleComplete(task.id)}
-                  >
-                    Marcar como Pendiente
-                  </Button>
+        completedTasks.map((task) => {
+          const { color } = getCategory(task.category_id);
+          return (
+            <Paper
+              key={task.id}
+              sx={{
+                width: "100%",
+                maxWidth: 800,
+                padding: 2,
+                marginBottom: 2,
+                textAlign: "left",
+                border: selectedTaskId === task.id ? "2px solid #1976d2" : "none",
+                backgroundColor: selectedTaskId === task.id ? "#e0f7fa" : color, 
+              }}
+            >
+              <Box sx={{ display: "flex", alignItems: "center" }}>
+                <Checkbox
+                  checked={selectedTaskId === task.id}
+                  onChange={() => handleSelectTask(task.id)}
+                  inputProps={{ "aria-label": "checkbox for completed task" }}
+                />
+                <Box sx={{ flexGrow: 1 }}>
+                  <Typography variant="h6">{task.title}</Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    {getCategory(task.category_id).name}:{" "}
+                    {task.description || "Sin descripción"}
+                  </Typography>
                 </Box>
-              )}
-            </Box>
-            
-          </Paper>
-        ))
+                {selectedTaskId === task.id && (
+                  <Box sx={{ display: "flex", gap: 1 }}>
+                    <Button
+                      variant="outlined"
+                      color="primary"
+                      onClick={() => handleEditClick(task.id)}
+                    >
+                      Editar
+                    </Button>
+                    <FormControl variant="outlined" size="small">
+                      <Select
+                        labelId={`status-select-label-${task.id}`}
+                        value={task.completed ? "completed" : "pending"}
+                        onChange={(e) =>
+                          handleChangeStatus(
+                            task.id,
+                            e.target.value === "completed"
+                          )
+                        }
+                      >
+                        {statusTasks.statuses.map((status) => (
+                          <MenuItem key={status.id} value={status.id}>
+                            {status.label}
+                          </MenuItem>
+                        ))}
+                      </Select>
+                    </FormControl>
+                  </Box>
+                )}
+              </Box>
+            </Paper>
+          );
+        })
       ) : (
         <Typography variant="body1">No hay tareas completadas.</Typography>
       )}
