4591557a2cd9277533ed77ce098e92f73d4cd705
leon_2192 <leon.strokes@gmail.com>
2024-11-01 16:46:36 -0300

    refactor

--- src/components/ui/TaskList/TaskList.tsx
+++ src/components/ui/TaskList/TaskList.tsx
@@ -18,17 +18,14 @@ const TaskList: React.FC = () => {
   const pendingTasks = tasks.filter((task) => !task.completed);
   const completedTasks = tasks.filter((task) => task.completed);
 
-  const findCategory = (categoryId: string) => {
-    return (
-      categories.find((cat) => cat.id === categoryId) || {
-        name: "Sin categoría",
-        color: "#f0f0f0",
-      }
-    );
-  };
+  const findCategory = (categoryId: string) =>
+    categories.find((cat) => cat.id === categoryId) || {
+      name: "Sin categoría",
+      color: "#f0f0f0",
+    };
 
   const handleSelectTask = (taskId: string) => {
-    setSelectedTaskId(selectedTaskId === taskId ? null : taskId);
+    setSelectedTaskId((prevId) => (prevId === taskId ? null : taskId));
   };
 
   const handleEditClick = (taskId: string) => {
@@ -36,13 +33,91 @@ const TaskList: React.FC = () => {
   };
 
   // Si se quiere probar la implementacion de delete se descomentan estas lineas de abajo
-  // y se debe traer del contexto la funcion handleDeleteTask
-  // y por ultimo descomentar el button que deje comentado en el return
-  // const handleDeleteClick = async (taskId: string) => {
+  //   y se debe traer del contexto la funcion handleDeleteTask
+  //    y por ultimo descomentar el button que deje comentado en el return
+  //    const handleDeleteClick = async (taskId: string) => {
   //   if (window.confirm("¿Estás seguro de que deseas eliminar esta tarea?")) {
-  //     await handleDeleteTask(taskId);
-  //   }
-  // };
+  //       await handleDeleteTask(taskId);
+  //   };
+
+  const renderTaskList = (taskList: typeof tasks, title: string) => (
+    <>
+      <Typography
+        variant="h5"
+        gutterBottom
+        sx={{
+          textAlign: "left",
+          marginBottom: 3,
+          maxWidth: 750,
+          width: "100%",
+        }}
+      >
+        {title}
+      </Typography>
+
+      {taskList.length > 0 ? (
+        taskList.map((task) => {
+          const { color } = findCategory(task.category_id);
+          const isSelected = selectedTaskId === task.id;
+
+          return (
+            <Paper
+              key={task.id}
+              sx={{
+                width: "100%",
+                maxWidth: 750,
+                padding: 1,
+                marginBottom: 2,
+                textAlign: "left",
+                border: isSelected ? "2px solid #1976d2" : "none",
+                backgroundColor: isSelected ? "#e3f2fd" : color,
+              }}
+            >
+              <Box sx={{ display: "flex", alignItems: "center" }}>
+                <Checkbox
+                  checked={isSelected}
+                  onChange={() => handleSelectTask(task.id)}
+                  inputProps={{ "aria-label": "checkbox for task" }}
+                />
+                <Box sx={{ flexGrow: 1 }}>
+                  <Typography variant="h6" sx={{ textAlign: "left" }}>
+                    {task.title}
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    {findCategory(task.category_id).name}:{" "}
+                    {task.description || "Sin descripción"}
+                  </Typography>
+                </Box>
+                {isSelected && (
+                  <Box sx={{ display: "flex", gap: 1 }}>
+                    <Button
+                      variant="outlined"
+                      color="primary"
+                      onClick={() => handleEditClick(task.id)}
+                    >
+                      Editar
+                    </Button>
+                    {/* <Button
+                      variant="outlined"
+                      color="error"
+                      onClick={() => handleDeleteClick(task.id)}
+                    >
+                      Eliminar
+                    </Button> */}
+                  </Box>
+                )}
+              </Box>
+            </Paper>
+          );
+        })
+      ) : (
+        <Typography variant="h5" align="left">
+          No hay tareas{" "}
+          {title === "Tareas Pendientes" ? "pendientes" : "completadas"}.
+        </Typography>
+      )}
+    </>
+  );
 
   return (
     <Box
@@ -59,154 +134,21 @@ const TaskList: React.FC = () => {
       <Box
         sx={{
           width: "100%",
-          maxWidth: 800,
+          maxWidth: 750,
           alignSelf: "center",
           textAlign: "left",
         }}
       >
-        <Typography variant="h4" gutterBottom>
+        <Typography variant="h3" gutterBottom>
           Lista de Tareas
         </Typography>
       </Box>
 
-      {/* Tareas Pendientes */}
-      <Box
-        sx={{
-          width: "100%",
-          maxWidth: 800,
-          alignSelf: "center",
-          textAlign: "left",
-        }}
-      >
-        <Typography variant="h5" gutterBottom>
-          Tareas Pendientes
-        </Typography>
-      </Box>
-      {pendingTasks.length > 0 ? (
-        pendingTasks.map((task) => {
-          const { color } = findCategory(task.category_id);
-          return (
-            <Paper
-              key={task.id}
-              sx={{
-                width: "100%",
-                maxWidth: 800,
-                padding: 2,
-                marginBottom: 2,
-                textAlign: "left",
-                border:
-                  selectedTaskId === task.id ? "2px solid #1976d2" : "none",
-                backgroundColor: selectedTaskId === task.id ? "#e3f2fd" : color,
-              }}
-            >
-              <Box sx={{ display: "flex", alignItems: "center" }}>
-                <Checkbox
-                  checked={selectedTaskId === task.id}
-                  onChange={() => handleSelectTask(task.id)}
-                  inputProps={{ "aria-label": "checkbox for task" }}
-                />
-                <Box sx={{ flexGrow: 1 }}>
-                  <Typography variant="h6">{task.title}</Typography>
-                  <Typography variant="body2" color="text.secondary">
-                    {findCategory(task.category_id).name}:{" "}
-                    {task.description || "Sin descripción"}
-                  </Typography>
-                </Box>
-                {selectedTaskId === task.id && (
-                  <Box sx={{ display: "flex", gap: 1 }}>
-                    <Button
-                      variant="outlined"
-                      color="primary"
-                      onClick={() => handleEditClick(task.id)}
-                    >
-                      Editar
-                    </Button>
-                    {/* <Button
-                      variant="outlined"
-                      color="error"
-                      onClick={() => handleDeleteClick(task.id)}
-                    >
-                      Eliminar
-                    </Button> */}
-                  </Box>
-                )}
-              </Box>
-            </Paper>
-          );
-        })
-      ) : (
-        <Typography variant="body1">No hay tareas pendientes.</Typography>
-      )}
+      {renderTaskList(pendingTasks, "Pendientes")}
 
       <Divider sx={{ my: 4 }} />
-      {/* Tareas Completadas */}
-      <Box
-        sx={{
-          width: "100%",
-          maxWidth: 800,
-          alignSelf: "center",
-          textAlign: "left",
-        }}
-      >
-        <Typography variant="h5" gutterBottom>
-          Tareas Completadas
-        </Typography>
-      </Box>
-      {completedTasks.length > 0 ? (
-        completedTasks.map((task) => {
-          const { color } = findCategory(task.category_id);
-          return (
-            <Paper
-              key={task.id}
-              sx={{
-                width: "100%",
-                maxWidth: 800,
-                padding: 2,
-                marginBottom: 2,
-                textAlign: "left",
-                border:
-                  selectedTaskId === task.id ? "2px solid #1976d2" : "none",
-                backgroundColor: selectedTaskId === task.id ? "#e0f7fa" : color,
-              }}
-            >
-              <Box sx={{ display: "flex", alignItems: "center" }}>
-                <Checkbox
-                  checked={selectedTaskId === task.id}
-                  onChange={() => handleSelectTask(task.id)}
-                  inputProps={{ "aria-label": "checkbox for completed task" }}
-                />
-                <Box sx={{ flexGrow: 1 }}>
-                  <Typography variant="h6">{task.title}</Typography>
-                  <Typography variant="body2" color="text.secondary">
-                    {findCategory(task.category_id).name}:{" "}
-                    {task.description || "Sin descripción"}
-                  </Typography>
-                </Box>
-                {selectedTaskId === task.id && (
-                  <Box sx={{ display: "flex", gap: 1 }}>
-                    <Button
-                      variant="outlined"
-                      color="primary"
-                      onClick={() => handleEditClick(task.id)}
-                    >
-                      Editar
-                    </Button>
-                    {/* <Button
-                      variant="outlined"
-                      color="error"
-                      onClick={() => handleDeleteClick(task.id)}
-                    >
-                      Eliminar
-                    </Button> */}
-                  </Box>
-                )}
-              </Box>
-            </Paper>
-          );
-        })
-      ) : (
-        <Typography variant="body1">No hay tareas completadas.</Typography>
-      )}
+
+      {renderTaskList(completedTasks, "Completadas")}
     </Box>
   );
 };
--- src/utilities/hooks/useGlobalContext.ts
+++ src/utilities/hooks/useGlobalContext.ts
@@ -4,7 +4,7 @@ import GlobalContext from "../../context/GlobalContext";
 export const useGlobalContext = () => {
   const context = useContext(GlobalContext);
   if (!context) {
-    throw new Error("useGlobalContext must be used within a GlobalProvider");
+    throw new Error("useGlobalContext ha obtenido un error.");
   }
   return context;
 };
