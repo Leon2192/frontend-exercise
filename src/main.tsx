a8dde0f417d91736cfa5e25bed5b2275b986f55c
leon2192 <leon.strokes@gmail.com>
2024-11-04 01:38:24 -0300

    fix quito strict mode para evitar solic http duplicada primer render

--- src/main.tsx
+++ src/main.tsx
@@ -5,9 +5,7 @@ import { GlobalProvider } from "./context/GlobalContext.tsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
     <GlobalProvider>
       <App />
     </GlobalProvider>
-  </React.StrictMode>
 );
